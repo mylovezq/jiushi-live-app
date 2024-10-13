1005fc97780fd933bf7c34d3bb586dea7e9e5c7b
jiushi <1083957956@qq.com>
2024-10-14 00:19:20 +0800

    优化发送im消息 redis

--- jiushi-live-framework/jiushi-live-framework-redis-starter/src/main/java/top/mylove7/jiushi/live/framework/redis/starter/config/RedisConfig.java
+++ jiushi-live-framework/jiushi-live-framework-redis-starter/src/main/java/top/mylove7/jiushi/live/framework/redis/starter/config/RedisConfig.java
@@ -1,7 +1,11 @@
 package top.mylove7.jiushi.live.framework.redis.starter.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -12,6 +16,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @ConditionalOnClass(RedisTemplate.class)
 public class RedisConfig {
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -26,4 +31,20 @@ public class RedisConfig {
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
+
+
+        @Bean
+        public RedissonClient redisson(RedisConnectionFactory connectionFactory) {
+            LettuceConnectionFactory lettuceConnectionFactory = (LettuceConnectionFactory) connectionFactory;
+            String hostName = lettuceConnectionFactory.getHostName();
+            int port = lettuceConnectionFactory.getPort();
+            String password = lettuceConnectionFactory.getPassword();
+            String redissonAddr = "redis://" + hostName + ":" + port;
+            Config config = new Config();
+            config.setCodec(new JsonJacksonCodec(new ObjectMapper()));
+            config.useSingleServer().setAddress(redissonAddr).setPassword(password);
+            return Redisson.create(config);
+
+        }
+
 }
